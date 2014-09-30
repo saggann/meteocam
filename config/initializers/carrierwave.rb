5afa51cdb64f1bdd16b8ff366c0f15ab02042469
Nico <saggann@gmail.com>
2014-09-30 13:20:06 +0200

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ require 'fog'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider                         => 'Google',
-    :google_storage_access_key_id     => 'GOOGFEZN4QWMGLHHQ654',
-    :google_storage_secret_access_key => 'uRNH/YYy3JPVesZTIi1HciauMyye1NPvDpw2+zxM'
+    :google_storage_access_key_id     => '',
+    :google_storage_secret_access_key => ''
   }
   config.fog_directory = 'meteocam'
 end
